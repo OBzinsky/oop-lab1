230770d738253235c79a66255d130bbb99cb1790
OliverBrzezinski-STU <G00475000@atu.ie>
2026-09-21 13:25:54 +0100

    Added Tracker

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -1,7 +1,8 @@
 package ie.atu.oop.week1;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         Book firstbook = new Book();
         firstbook.title = "Dune";
@@ -13,5 +14,25 @@ public class Main {
         firstbook.borrowBook();
         // after loan
         firstbook.displayDetails();
+
+        Book secondbook = createBook("Clean Code",  "Dan Williams", 223);
+        Book thirdbook = createBook("Another Book Code",  "John Murphy", 133);
+        Book fourthbook = createBook("Student Life", "Mary Shelly", 442);
+
+        System.out.println("\n");
+        secondbook.displayDetails();
+        System.out.println("\n");
+        thirdbook.displayDetails();
+        System.out.println("\n");
+        fourthbook.displayDetails();
+    }
+
+    private static Book createBook(String title, String author, int pageCount)
+    {
+        Book book = new Book();
+        book.title = title;
+        book.author = author;
+        book.pageCount = pageCount;
+        return book;
     }
 }
