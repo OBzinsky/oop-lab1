1deb15246e070dc4b2ae5ad5363e6e7a535041bd
OliverBrzezinski-STU <G00475000@atu.ie>
2026-09-21 12:57:28 +0100

    Added displayDetails method to book

--- src/main/java/ie/atu/oop/week1/Book.java
+++ src/main/java/ie/atu/oop/week1/Book.java
@@ -1,10 +1,16 @@
 package ie.atu.oop.week1;
 
 public class Book {
-    @Override
+
     public String title;
     public String author;
     public int pageCount;
     public boolean available = true;
+
+    public void displayDetails() {
+        System.out.println("Book title: " + title);
+        System.out.println("Book author: " + author);
+        System.out.println("Book page count: " + pageCount);
+        System.out.println("Book available: " + available);
     }
 }
